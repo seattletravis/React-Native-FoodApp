89afc8461e23ce55782f81d5721ee3ffc59fcb36
Travis Lamberte <travis@lamberte.com>
2023-11-04 08:19:02 -0700

    wire up yelp api to search

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -5,17 +5,29 @@ import yelp from '../api/yelp'
 
 const SearchScreen = () => {
   const [term, setTerm] = useState('');
-  const [result, setResult] = useState([])
+  const [results, setResults] = useState([])
+
+  const searchApi = async () => {
+    const response = await yelp.get('/search', {
+      params: {
+        limit: 50,
+        term: term,
+        location: 'seattle'
+      }
+    });
+    setResults(response.data.businesses)
+  }
+
 
   return (
     <View>
       <SearchBar 
         term={term} 
-        onTermChange={(newTerm) => setTerm(newTerm)} 
-        onTermSubmit={() => console.log('term was submitted')}
+        onTermChange={setTerm} 
+        onTermSubmit={searchApi}
       />
       <Text>Search Screen</Text>
-      <Text>{term}</Text>
+      <Text>We have found {results.length} results</Text>
     </View>
   );
 };
