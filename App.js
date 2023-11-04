01082b6423f87beda8be86cb684e28652d382417
Travis Lamberte <travis@lamberte.com>
2023-11-03 17:31:35 -0700

    add styling to searchbar

--- App.js
+++ App.js
@@ -1,6 +1,5 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-
 import SearchScreen from './src/screens/SearchScreen';
 
 const navigator = createStackNavigator(
