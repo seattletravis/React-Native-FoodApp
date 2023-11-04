4764091ab25482ebca93edd2102acd1903f8fd35
Travis Lamberte <travis@lamberte.com>
2023-11-04 07:44:39 -0700

    add yelp api

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,13 +2,18 @@ import React from 'react';
 import { View, TextInput, StyleSheet } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
-const SearchBar = () => {
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
   return (
     <View style={styles.backgroundStyle}>
         <Feather name='search' style={styles.iconStyle} />     
       <TextInput 
+        autoCapitalize="none"
+        autoCorrect={false}
         style={styles.inputStyle}
         placeholder="Search"
+        value={term}
+        onChangeText={onTermChange}
+        onEndEditing={onTermSubmit}
       />
     </View>
   );
@@ -19,19 +24,19 @@ const styles = StyleSheet.create({
         backgroundColor: '#F0EEEE',
         height: 50,
         borderRadius: 10,
+        marginTop: 10,
         marginHorizontal: 15,
         flexDirection: 'row',
     },
     inputStyle: {
-
         flex: 1,
+        fontSize: 18,
     },
     iconStyle: {
         fontSize: 35,
         alignSelf: 'center',
         marginHorizontal: 15,
     }
-
 });
 
 export default SearchBar;
