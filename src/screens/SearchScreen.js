b5b90eb1de2ccb327cc71978f5c08f265a84acba
Travis Lamberte <travis@lamberte.com>
2023-11-04 09:25:07 -0700

    add initial searchTerm

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
 import yelp from '../api/yelp'
@@ -8,12 +8,12 @@ const SearchScreen = () => {
   const [results, setResults] = useState([])
   const [errorMessage, setErrorMessage] = useState('')
 
-  const searchApi = async () => {
+  const searchApi = async (searchTerm) => {
   try {
       const response = await yelp.get('/search', {
         params: {
           limit: 50,
-          term: term,
+          term: searchTerm,
           location: 'seattle'
         }
       });
@@ -29,7 +29,7 @@ const SearchScreen = () => {
       <SearchBar 
         term={term} 
         onTermChange={setTerm} 
-        onTermSubmit={searchApi}
+        onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results</Text>
