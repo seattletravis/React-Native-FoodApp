8dbe57c7db2a0e3fa6d5e450aae409538c09c6bc
Travis Lamberte <travis@lamberte.com>
2023-11-06 09:06:16 -0800

    add ResultsList component to show result cards

---
+++ src/components/ResultsList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import {View, Text, StyleSheet } from 'react-native'
+
+const ResultsList = () => {
+    return(
+        <View>
+            <Text>Result List</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+
+})
+
+export default ResultsList
