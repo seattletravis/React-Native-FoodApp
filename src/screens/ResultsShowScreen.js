a3f4fcb8d8d0c158997b09dad2c5ccad70595e02
Travis Lamberte <travis@lamberte.com>
2023-11-06 13:03:13 -0800

    add useEffect hook to call getResult

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 import yelp from '../api/yelp'
 
@@ -11,10 +11,10 @@ const ResultsShowScreen = ({ navigation }) => {
         setResult(response.data)
     }
 
+    useEffect(() => {
+        getResult(id)
+    },[]) 
     
-
-
-
     return (
         <View> 
             <Text> 
