9684d7884cc1d6c854d4f7636462d0b7738d8e7a
Travis Lamberte <travis@lamberte.com>
2023-11-06 12:15:14 -0800

    remove horizontal scroll indicator

--- src/components/ResultsDetail.js
+++ src/components/ResultsDetail.js
@@ -7,7 +7,6 @@ const ResultsDetail = ({ result }) => {
             <Image style={styles.image} source={{ uri: result.image_url }}/>
             <Text style={styles.name}>{result.name}</Text>
             <Text style={styles.stars}>{result.rating} Stars, {result.review_count} Reviews</Text>
-
         </View>
     )
 }
@@ -20,6 +19,7 @@ const styles = StyleSheet.create({
         width: 250,
         height: 120,
         borderRadius: 4,
+        marginBottom: 5,
     },
     name: {
         fontWeight: 'bold',
--- src/components/ResultsList.js
+++ src/components/ResultsList.js
@@ -9,6 +9,7 @@ const ResultsList = ({ title, results }) => {
             <Text style={styles.title}> {title}</Text>
             <FlatList
                 horizontal
+                showsHorizontalScrollIndicator={false}
                 data={results}
                 keyExtractor={(result) => result.id}
                 renderItem={({ item }) => {
@@ -23,7 +24,8 @@ const styles = StyleSheet.create({
     title: {
         fontSize: 18,
         fontWeight: 'bold',
-        marginLeft: 15
+        marginLeft: 15,
+        marginBottom: 5,
     },
     container: {
         marginBottom: 10,
